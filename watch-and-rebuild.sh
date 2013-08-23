624d3970fdae07071d11f8ab973aef0d0078c02f
Linus <anonymouse2048@gmail.com>
2013-08-23 13:34:55 +0200

    Add automatic reloading to watching script

--- watch-and-rebuild.sh
+++ watch-and-rebuild.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+echo $0
+touch /tmp/war-date
 while true ; do
 	sleep 1;
 	if [ /tmp/fxsdk-updated -nt /tmp/fxsdk-rebuilt ] ; then
+		( # New scope (so we automatically go back to the old wd afterwards)
 		echo "Rebuilding."
 		cd ~/shared/cp-program/fxsdk
 		git pull
@@ -10,5 +13,9 @@ while true ; do
 		cmake ..
 		make
 		touch /tmp/fxsdk-rebuilt
+		)
+	fi
+	if [ "$0" -nt /tmp/war-date ] ; then
+		exec $0 # Basically, reload the script
 	fi
 done
